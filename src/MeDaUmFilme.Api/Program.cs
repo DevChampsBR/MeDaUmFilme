a9ffca78bd9f28a2e424257ae5888713e4d6666b
Giovanni Bassi <giggio@giggio.net>
2016-12-02 15:39:18 -0200

    Change to console app

--- src/MeDaUmFilme.Api/Program.cs
+++ src/MeDaUmFilme.Api/Program.cs
@@ -3,23 +3,51 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MeDaUmFilme.Language;
 
 namespace MeDaUmFilme.Api
 {
     public class Program
     {
+        private static IAnalyzer languageAnalyzer;
+        private static IMeDaUmFilmeSearch meDaUmFilmeSearch;
+
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+            var env = Environment.GetEnvironmentVariable("TWITTER_ENV") ?? "Development";
+            var cwd = Directory.GetCurrentDirectory();
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(cwd)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env}.json", optional: true)
+                .AddEnvironmentVariables();
+            var configuration = builder.Build();
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddLanguage(configuration.GetSection("Language"));
+            serviceCollection.AddTwitter(configuration.GetSection("Twitter"));
+            serviceCollection.AddMeDaUmFilme();
+            var services = serviceCollection.BuildServiceProvider();
+            languageAnalyzer = services.GetService<IAnalyzer>();
+            meDaUmFilmeSearch = services.GetService<IMeDaUmFilmeSearch>();
+            var twitter = services.GetService<Twitter.Twitter>();
+            twitter.ListenAsync("@medaumfilme", Listen).Wait();
+        }
 
-            host.Run();
+        private async static void Listen(string tweet)
+        {
+            Console.WriteLine($"Searched: {tweet}");
+            var intent = await languageAnalyzer.AnalyzeAsync(tweet);
+            if (intent.Name == "BuscaTitulo")
+            {
+                var omdbRequest = new OmbdRequest()
+                {
+                    Title = intent.Entities["Titulo"]
+                };
+                var movie = await meDaUmFilmeSearch.GetMovie(omdbRequest);
+                Console.WriteLine($"Found: {movie.Title} from {movie.Year}");
+            }
         }
     }
 }
--- src/MeDaUmFilme.Twitter/Twitter.cs
+++ src/MeDaUmFilme.Twitter/Twitter.cs
@@ -15,13 +15,28 @@ namespace MeDaUmFilme.Twitter
             Auth.SetUserCredentials(config.ConsumerKey, config.ConsumerSecret, config.AccessToken, config.AccessTokenSecret);
         }
 
-        public Task Listen(string searchTerm, Action<string> received)
+        public Task ListenAsync(string searchTerm, Action<string> received)
         {
             var stream = Stream.CreateFilteredStream();
             stream.AddTrack(searchTerm);
             stream.MatchingTweetReceived += (sender, args) =>
             {
-                received(args.Tweet.FullText);
+                var fullText = args.Tweet.FullText;
+                var mentions = args.Tweet.UserMentions;
+                var removedCount = 0;
+                var lastIndex = 0;
+                foreach (var mention in mentions)
+                {
+                    for (int i = 0; i < mention.Indices.Count; i += 2)
+                    {
+                        var firstPart = fullText.Substring(lastIndex - removedCount, mention.Indices[i] - removedCount);
+                        var secondPart = fullText.Substring(mention.Indices[i + 1] - removedCount);
+                        fullText = firstPart + secondPart;
+                        lastIndex = mention.Indices[i + 1];
+                        removedCount += mention.Indices[i + 1] - mention.Indices[i];
+                    }
+                }
+                received(fullText);
             };
             return stream.StartStreamMatchingAllConditionsAsync();
         }
